7ddfe26e58fcdd2dbcd5ffa9e2aa0a9e620494da
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-05-19 12:44:11 +0300

    add logo to header

--- src/components/Common/Header/Header.component.tsx
+++ src/components/Common/Header/Header.component.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import classNames from "classnames";
 
 import styles from "./Header.module.scss";
+import Image from "next/image";
 
 export const HeaderComponent = () => {
   const headerClassName = classNames(`${styles.header} container`);
 
-  return <header className={headerClassName}>header</header>;
+  return (
+    <header className={headerClassName}>
+      <div className={styles.logoContainer}>
+        <Image
+          src={"/images/logo.png"}
+          fill
+          sizes='100vw'
+          alt='productImg'
+          className={styles.productImg}
+        />
+      </div>
+    </header>
+  );
 };
 
 HeaderComponent.displayName = "Header";
