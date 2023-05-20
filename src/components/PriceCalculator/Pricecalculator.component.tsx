91b5a77c87193ca12298c731352b1cc2bdffa4cf
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-05-21 01:42:40 +0300

    updated calculator styles, add reset button

--- src/components/PriceCalculator/Pricecalculator.component.tsx
+++ src/components/PriceCalculator/Pricecalculator.component.tsx
@@ -3,15 +3,21 @@ import { useState } from "react";
 import { Input } from "@components/UI/Input";
 
 import styles from "./PriceCalculator.module.scss";
+import { Button, ButtonVariantEnum } from "@components/UI/Button";
 
 export const PriceCalculatorComponent = () => {
-  const [goldAmount, setGoldAmount] = useState(1);
-  const [price, setPrice] = useState(1);
+  const [goldAmount, setGoldAmount] = useState(0);
+  const [price, setPrice] = useState(0);
 
   const handleChangeGoldAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
     const amount = parseInt(e.target.value);
     setGoldAmount(amount);
     setPrice(amount * 10);
+  };
+
+  const handleResetValues = () => {
+    setGoldAmount(0);
+    setPrice(0);
   };
 
   return (
@@ -20,12 +26,26 @@ export const PriceCalculatorComponent = () => {
         type='number'
         id='goldAmount'
         value={goldAmount || ""}
+        label='Enter gold amount'
         onChange={handleChangeGoldAmount}
-        placeholder='Amount of gold'
         hasError={goldAmount < 0}
         errorMessage={goldAmount < 0 ? "Negative numbers >_<" : ""}
+        style={{ width: "fit-content" }}
       />
-      <Input id='price' value={`USD: ${price || ""}`} disabled />
+      <Input
+        id='price'
+        label='Price'
+        value={goldAmount > 0 ? `USD: ${price || ""}` : ""}
+        disabled
+        style={{ width: "fit-content" }}
+      />
+      <Button
+        text='Reset'
+        variant={ButtonVariantEnum.text}
+        width='full'
+        className={styles.resetButton}
+        onClick={handleResetValues}
+      />
     </div>
   );
 };
